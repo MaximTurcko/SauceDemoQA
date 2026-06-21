f69462c2838bf88c736d5ab3691d229bb4ab522d
MaximTurcko <max.turcko@yandex.ru>
2026-06-21 19:16:29 +0300

    Added CartTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,8 +1,8 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
 
@@ -11,13 +11,15 @@ public class BaseTest {
 
     @BeforeMethod
     public void setup() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
     }
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
-        //driver.quit();
+        driver.quit();
     }
 }
--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -1,3 +1,24 @@
-public class CartTest extends BaseTest{
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
+public class CartTest extends BaseTest {
+
+    @Test
+    public void checkCart() {
+        driver.get("https://www.saucedemo.com");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(driver.findElement(By.cssSelector(".title")).getText(), "Products",
+                "Логин не выполнен");
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        softAssert.assertEquals(driver.findElement(By.id("remove-sauce-labs-backpack")).getText(), "Remove",
+                "Кнопка добавления в карзину не нажата");
+        driver.findElement(By.cssSelector(".shopping_cart_link")).click();
+        softAssert.assertEquals(driver.findElement(By.cssSelector(".inventory_item_price")).getText(), "$29.99",
+                "Не правильная сумма");
+        softAssert.assertAll();
+    }
 }
