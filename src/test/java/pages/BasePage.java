f99252360eefc0b4f7bd222957db19c9da53516e
MaximTurcko <max.turcko@yandex.ru>
2026-06-26 14:11:22 +0300

    Added method isElementPresent in BasePage

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,6 +1,12 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.time.Duration;
+import java.util.List;
 
 public abstract class BasePage {
     WebDriver driver;
@@ -8,5 +14,12 @@ public abstract class BasePage {
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public void isElementPresent(By locator){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+        List<WebElement> elements = driver.findElements(locator);
+        Assert.assertEquals(elements.size(), 0);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 }
